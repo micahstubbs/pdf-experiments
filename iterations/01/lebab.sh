486c627cf7f67fe64e78eed5aa23d337ac8c11d6
micahstubbs <micah.stubbs@gmail.com>
2019-10-19 00:59:28 -0700

    index.js

--- iterations/01/lebab.sh
+++ iterations/01/lebab.sh
@@ -1,12 +1,12 @@
 # safe
-lebab --replace vis.js --transform arrow
-lebab --replace vis.js --transform for-of
-lebab --replace vis.js --transform for-each
-lebab --replace vis.js --transform arg-rest
-lebab --replace vis.js --transform arg-spread
-lebab --replace vis.js --transform obj-method
-lebab --replace vis.js --transform obj-shorthand
-lebab --replace vis.js --transform multi-var
+lebab --replace index.js --transform arrow
+lebab --replace index.js --transform for-of
+lebab --replace index.js --transform for-each
+lebab --replace index.js --transform arg-rest
+lebab --replace index.js --transform arg-spread
+lebab --replace index.js --transform obj-method
+lebab --replace index.js --transform obj-shorthand
+lebab --replace index.js --transform multi-var
 # unsafe
-lebab --replace vis.js --transform let
-lebab --replace vis.js --transform template
+lebab --replace index.js --transform let
+lebab --replace index.js --transform template
